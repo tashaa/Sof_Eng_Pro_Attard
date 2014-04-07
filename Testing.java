51d6067a2adfdfb6ec9416fa5f0a05e936d1b606
tashaa <tashaa28293@gmail.com>
2014-04-07 21:46:01 +0200

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -2,14 +2,23 @@ package attardattard;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Testing {
+	
+	private Main h;
+	 @Before
+	   public void setUp() throws Exception 
+	   {
+	      h = new Main();
+	   }
 
 	@Test
-	public void test() {
-		Main x = new Main();
-		assertEquals(5,x.numberRet());
-	}
+	   public void testHelloEmpty() 
+	   {
+	      assertEquals(h.getName(),"");
+	      assertEquals(h.getMessage(),"Hello!");
+	   }
 
 }
