0e70116b8faa546f41516db9826deb10b0f50ec4
tashaa <tashaa28293@gmail.com>
2014-04-07 21:35:16 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,27 @@ package attardattard;
 
 public class Main {
 	
-	int numberRet(){
-		return 5;
-	}
+   private String name = "";
+
+   public String getName() 
+   {
+      return name;
+   }
+
+   public String getMessage() 
+   {
+      if (name == "") 
+      {
+         return "Hello!";
+      }
+      else 
+      {
+         return "Hello " + name + "!";
+      }
+   }
+
+   public void setName(String name) 
+   {
+      this.name = name;
+   }
 }
